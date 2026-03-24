570d1962f97bb93c900762224b4ef78b3e2cfa3d
Aleksey Kovakin <qwazik21@gmail.com>
2026-03-24 12:08:13 +0300

    Enhance restart script: add git fetch and pull commands to ensure the latest changes are integrated before stopping and removing containers.

--- restart.sh
+++ restart.sh
@@ -1,3 +1,4 @@
+git fetch && git pull
 # 1) Остановить и удалить контейнеры этого compose-проекта
 docker compose down --remove-orphans
 # 2) Пересобрать образы без кэша (и с обновлением базового образа)
