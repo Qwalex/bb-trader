6a0b9c110a70e4b564e3334ba5fc6306705ef810
Aleksey Kovakin <qwazik21@gmail.com>
2026-03-24 12:13:47 +0300

    Enhance restart script: add git reset command for a clean state before pulling updates, and integrate a notification system to alert on project updates via a POST request.

--- restart.sh
+++ restart.sh
@@ -1,7 +1,15 @@
-git fetch && git pull
+git fetch
+git reset --hard HEAD && git pull
 # 1) Остановить и удалить контейнеры этого compose-проекта
 docker compose down --remove-orphans
 # 2) Пересобрать образы без кэша (и с обновлением базового образа)
 docker compose build --no-cache --pull
 # 3) Поднять заново
 docker compose up -d
+text="Проект bb-trade обновлен"
+curl -X POST "https://dev.qwalex.ru/notify/" \
+  -H "Content-Type: application/json" \
+  -d "{\"text\":\"$text\"}" \
+  --fail \
+  --show-error \
+  --silent || echo "curl error"
